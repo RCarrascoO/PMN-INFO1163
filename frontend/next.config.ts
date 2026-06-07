8e360bd70ca7d27681aaf926ce2886871750a992
RCarrascO <renato.carrasco2024@alu.uct.cl>
2026-06-07 16:24:20 -0400

    hotfix Servidor

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   devIndicators: false,
+  allowedDevOrigins: ['lsanehost.zapto.org'], // <- Línea agregada
   env: {
     NEXT_PUBLIC_API_URL: "http://localhost:8000",
   },
